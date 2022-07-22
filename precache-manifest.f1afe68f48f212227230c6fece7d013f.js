73df9930ef04c738983242fa82f2a130655e5839
Rereyre <rereyre.rere@gmail.com>
2022-07-22 17:26:17 +0700

    Updates

---
+++ precache-manifest.f1afe68f48f212227230c6fece7d013f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "838256bcec72064fab6e",
+    "url": "/reactjs/static/css/main.28f3b375.chunk.css"
+  },
+  {
+    "revision": "838256bcec72064fab6e",
+    "url": "/reactjs/static/js/main.838256bc.chunk.js"
+  },
+  {
+    "revision": "94421d0c55535ff39cc4",
+    "url": "/reactjs/static/js/1.94421d0c.chunk.js"
+  },
+  {
+    "revision": "7035acbc7f79d1475900",
+    "url": "/reactjs/static/css/2.3c90d1ed.chunk.css"
+  },
+  {
+    "revision": "7035acbc7f79d1475900",
+    "url": "/reactjs/static/js/2.7035acbc.chunk.js"
+  },
+  {
+    "revision": "c00f5d678e1d50e0fe76",
+    "url": "/reactjs/static/js/runtime~main.c00f5d67.js"
+  },
+  {
+    "revision": "8d4a56b389ae1f50170592809197047e",
+    "url": "/reactjs/static/media/women.8d4a56b3.jpeg"
+  },
+  {
+    "revision": "57b8ef0b526148101f0fde60d56b63c0",
+    "url": "/reactjs/static/media/tugu.57b8ef0b.jpg"
+  },
+  {
+    "revision": "134fff0461fabb63f68a965f15cb6605",
+    "url": "/reactjs/static/media/subway.134fff04.jpeg"
+  },
+  {
+    "revision": "9a87d3dad544a1201c5dad1af4b0586d",
+    "url": "/reactjs/static/media/boy.9a87d3da.jpeg"
+  },
+  {
+    "revision": "9067ccfd433de51d8ab1243b67bf5426",
+    "url": "/reactjs/index.html"
+  }
+];
